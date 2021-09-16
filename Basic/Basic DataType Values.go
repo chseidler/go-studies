38207ef17aa1581d8f9ce0b8e5d4d87e3472da08
Christian Seidler <ch.cseidler@gmail.com>
2021-09-16 12:24:48 -0300

    change dir name from hello world to basic types of values inserting comments in the code push and commit directly from GoLand

---
+++ Basic/Basic DataType Values.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go" + "language") // string
+	fmt.Println(2)                 // int
+	fmt.Println(55.5)              //float
+	fmt.Println(true && false)     //boolean
+
+}
